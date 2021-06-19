ff02d2d5be01e217bf7a221e7f92abbe49995ef2
GuhSerafs <gustavo_duarte200@hotmail.com>
2021-06-19 01:15:47 -0300

    Revert "Inicializando o projeto do servidor http."
    This reverts commit 1eae90f76fd5ecd76c0e73b0f0d67d9e4a6bd115.

--- http-server/target/debug/http-server.d
+++
@@ -1,1 +0,0 @@
-D:\Workspaces\rust\rust-course-repo\http-server\target\debug\http-server.exe: D:\Workspaces\rust\rust-course-repo\http-server\src\main.rs
--- http-server/target/rls/debug/deps/http_server-db93ebb9ba4fbbb7.d
+++
@@ -1,5 +0,0 @@
-d:\Workspaces\rust\rust-course-repo\http-server\target\rls\debug\deps\http_server-db93ebb9ba4fbbb7.rmeta: src\main.rs
-
-d:\Workspaces\rust\rust-course-repo\http-server\target\rls\debug\deps\http_server-db93ebb9ba4fbbb7.d: src\main.rs
-
-src\main.rs:
